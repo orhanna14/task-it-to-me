8010a765042b57ab8631855256b4ea1aeb7f7f86
Olivia McGoffin <olivia@thoughtbot.com>
2020-04-30 11:02:07 -0700

    Move escape characters from project_printer to colors

---
+++ lib/colors.rb
@@ -0,0 +1,13 @@
+class Colors
+  def orange
+    "\e[40;38;5;214m"
+  end
+
+  def green
+    "\e[38;5;40m"
+  end
+
+  def escape_color
+    "\e[0m"
+  end
+end
--- lib/project_printer.rb
+++ lib/project_printer.rb
@@ -1,48 +1,51 @@
+require_relative "colors"
+
 class ProjectPrinter
-  attr_accessor :stdout
+  attr_accessor :stdout, :colors
 
   def initialize(stdout)
     @stdout = stdout
+    @colors = Colors.new
   end
 
   def projects(project)
-    stdout.puts("  #{project.keys.first}\n")
+    stdout.puts("  #{project.keys.first}")
   end
 
   def deleting_a_project(project_name)
-    stdout.puts "\e[38;5;40mDeleting project:\e[0m '#{project_name.strip}'\n\n"
+    stdout.puts colors.green + "Deleting project: '#{project_name.strip}'" + colors.escape_color
   end
 
   def does_not_exist(project_name)
-    stdout.puts "\e[40;38;5;214mProject doesn't exist:\e[0m '#{project_name.strip}'\n\n"
+    stdout.puts colors.orange + "Project doesn't exist: '#{project_name.strip}'" + colors.escape_color
   end
 
   def created(name)
-    stdout.puts("\e[38;5;40mCreated project:\e[0m '#{name}'\n\n")
+    stdout.puts colors.green + ("Created project: '#{name}'") + colors.escape_color
   end
 
   def changed_name(old_name, new_name)
-    stdout.puts("\e[38;5;40mChanged project name from\e[0m '#{old_name}' \e[38;5;40mto\e[0m '#{new_name}'\n\n")
+    stdout.puts colors.green + ("Changed project name from '#{old_name}' to '#{new_name}'") + colors.escape_color
   end
 
   def list_of_projects
-    stdout.puts("\e[38;5;40mListing projects:\e[0m\n")
+    stdout.puts colors.green + ("Listing projects:") + colors.escape_color
   end
 
   def none_created
-    stdout.puts("\e[40;38;5;214mNo projects created\e[0m\n\n")
+    stdout.puts colors.orange + ("No projects created\n\n") + colors.escape_color
   end
 
   def cannot_delete_a_project
-    stdout.puts("\e[40;38;5;214mCan't delete a project\e[0m")
+    stdout.puts colors.orange + ("Can't delete a project") + colors.escape_color
   end
 
   def cannot_edit_a_project
-    stdout.puts("\e[40;38;5;214mCan't edit project\e[0m")
+    stdout.puts colors.orange + ("Can't edit project") + colors.escape_color
   end
 
   def cannot_edit_projects
-    stdout.puts("\e[40;38;5;214mCan't edit any projects\e[0m")
+    stdout.puts colors.orange + ("Can't edit any projects") + colors.escape_color
   end
 
   def single_line
